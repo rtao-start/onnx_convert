66ecdc00e7bd66523a0b31bdd7ccac644d17c36c
ahqzy <ahqzy@163.com>
2022-07-05 15:46:56 +0800

    test custom op

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-#import onnx
+'''
 from onnx import load_model, save_model
 import torch
 import torch.nn as nn
@@ -55,7 +55,9 @@ trans_model = convert_float_to_float16(onnx_model,keep_io_types=True)
 
 #save_model(trans_model, "test_net_fp16.onnx")
 save_model(trans_model, "fp16.onnx")
+'''
 
+################ extract_model
 '''
 input_path = './output.onnx'
 output_path = './my.onnx'
@@ -64,3 +66,105 @@ output_names = ['functional_1/concatenate/concat:0']
 
 onnx.utils.extract_model(input_path, output_path, input_names, output_names)
 '''
+
+######## insert preprocess node
+'''
+import onnx
+
+onnx_model = onnx.load('./v4_sub.onnx')
+graph = onnx_model.graph
+
+input_name = graph.input[0].name
+
+sub_const_node = onnx.helper.make_tensor(name='const_sub',
+                      data_type=onnx.TensorProto.FLOAT,
+                      dims=[3],
+                      vals=[-127.5, -127.5, -127.5])
+
+graph.initializer.append(sub_const_node)
+
+sub_node = onnx.helper.make_node(
+                'Add',
+                name='sub',
+                inputs=[input_name, 'const_sub'],
+                outputs=['pre_sub'])
+
+graph.node.insert(0, sub_node)
+
+mul_const_node = onnx.helper.make_tensor(name='const_mul',
+                      data_type=onnx.TensorProto.FLOAT,
+                      dims=[3],
+                      vals=[1.0/127.5, 1.0/127.5, 1.0/127.5])
+
+graph.initializer.append(mul_const_node)
+
+sub_node = onnx.helper.make_node(
+               'Mul',
+               name='mul',
+               inputs=['pre_sub', 'const_mul'],
+               outputs=['pre_mul'])
+
+graph.node.insert(1, sub_node)
+
+graph.node[2].input[0]='pre_mul'
+
+graph = onnx.helper.make_graph(graph.node, graph.name, graph.input, graph.output, graph.initializer)
+info_model = onnx.helper.make_model(graph)
+onnx_model = onnx.shape_inference.infer_shapes(info_model)
+ 
+onnx.checker.check_model(onnx_model)
+onnx.save(onnx_model, './nn.onnx')
+'''
+
+######## insert preprocess node 2
+import onnx
+
+onnx_model = onnx.load('./v4_sub.onnx')
+graph = onnx_model.graph
+
+input_name = graph.input[0].name
+
+mean_const_node = onnx.helper.make_tensor(name='const_mean',
+                      data_type=onnx.TensorProto.FLOAT,
+                      dims=[3],
+                      vals=[127.5, 127.5, 127.5])
+
+graph.initializer.append(mean_const_node)
+
+std_const_node = onnx.helper.make_tensor(name='const_std',
+                      data_type=onnx.TensorProto.FLOAT,
+                      dims=[3],
+                      vals=[1.0/127.5, 1.0/127.5, 1.0/127.5])
+
+graph.initializer.append(std_const_node)
+
+resize_const_node = onnx.helper.make_tensor(name='const_size',
+                      data_type=onnx.TensorProto.FLOAT,
+                      dims=[2],
+                      vals=[224, 224])
+
+graph.initializer.append(resize_const_node)
+
+crop_const_node = onnx.helper.make_tensor(name='const_crop',
+                      data_type=onnx.TensorProto.FLOAT,
+                      dims=[2],
+                      vals=[12, 12])
+
+graph.initializer.append(crop_const_node)
+
+pre_process_node = onnx.helper.make_node(
+                'PreProc',
+                name='preprocess',
+                inputs=[input_name, 'const_mean', 'const_std', 'const_size','const_crop'],
+                outputs=['pre_process'])
+
+graph.node.insert(0, pre_process_node)
+
+graph.node[1].input[0]='pre_process'
+
+graph = onnx.helper.make_graph(graph.node, graph.name, graph.input, graph.output, graph.initializer)
+info_model = onnx.helper.make_model(graph)
+onnx_model = onnx.shape_inference.infer_shapes(info_model)
+ 
+#onnx.checker.check_model(onnx_model)
+onnx.save(onnx_model, './mm.onnx')
