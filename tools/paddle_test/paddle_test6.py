9e220c9707ab210a5977d3ada215bf68d0a91e07
ahqzy <ahqzy@163.com>
2022-09-07 16:22:02 +0800

    add tools for debug

---
+++ tools/paddle_test/paddle_test6.py
@@ -0,0 +1,61 @@
+import paddle
+import paddle.nn as nn
+import numpy as np
+
+__all__ = []
+
+
+class LeNet(nn.Layer):
+    """LeNet model from
+    `"LeCun Y, Bottou L, Bengio Y, et al. Gradient-based learning applied to document recognition[J]. Proceedings of the IEEE, 1998, 86(11): 2278-2324.`_
+
+    Args:
+        num_classes (int): output dim of last fc layer. If num_classes <=0, last fc layer 
+                            will not be defined. Default: 10.
+
+    Examples:
+        .. code-block:: python
+
+            from paddle.vision.models import LeNet
+
+            model = LeNet()
+    """
+
+    def __init__(self, num_classes=10):
+        super(LeNet, self).__init__()
+        self.num_classes = num_classes
+        self.features = nn.Sequential(
+            nn.Conv2D(
+                1, 6, 3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2D(2, 2),
+            nn.Conv2D(
+                6, 16, 5, stride=1, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2D(2, 2))
+
+        if num_classes > 0:
+            self.fc = nn.Sequential(
+                nn.Linear(400, 120),
+                nn.Linear(120, 84), nn.Linear(84, num_classes))
+
+    def forward(self, inputs):
+        x = self.features(inputs)
+
+        if self.num_classes > 0:
+            x = paddle.flatten(x, 1)
+            x = self.fc(x)
+        return x
+
+
+##################################################################################################################
+##################################################################################################################
+
+model = LeNet()
+
+x = np.random.random((1, 1, 28, 28)).astype('float32')
+
+output = model(paddle.to_tensor(x))
+
+print('output:', output)
+
